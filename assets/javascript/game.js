4cfaa411ec201aac1aa62eddeedfe0b775d79678
Kai <huikaizhong@yahoo.com>
2016-07-22 23:35:40 -0400

    in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,42 @@
 $( document ).ready(function() {
-	
+	//choose character
+	$(".thumbnail").on("click", function() {
+        $(this).remove();
+        $("#yourCharacter").append(this);
+        //the rest become enemies
+     	$("#chooseCharacter").children().each(function () {
+        $(this).remove();
+        $("#enemies").append(this);
+        $(".thumbnail").addClass("options");
+        });
+    //the chosen one becomes defender
+    $(".options").on("click", function() {
+     	$(this).remove();
+     	$("#defender").append(this);
+   });
 
-	$( '.pic' ).on('click', function() {
-		
-		$(this).detach();
-		$(this).append(".col-xs-6 col-md-1 one");
-
-	});
 
 
 
 
+
+
+	//When click "Attack" button:
+   // $('.attack').on('click',function() {
+    	//var HP += 6;
+    	
+    	//if (HP<=0){
+    		//print("You been defeated! Game over!");
+    	//}
+    	//else {
+    		//$(".displayText").html("You attacked "+    + "for "+    + "damage.");
+    	//}
+    //});
+
+
+    //Reset button for a new game
+    $(".reset").on("click", function() {
+ 		location.reload();
+    });
+});
 });
